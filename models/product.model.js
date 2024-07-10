498e84c32ff25f74fdb7d7d80b0dd56b9b9edc23
Aishwary Dandale <aishdandale7@gmail.com>
2024-07-10 20:10:53 +0530

    product model

--- models/product.model.js
+++ models/product.model.js
@@ -3,3 +3,29 @@
  * name , price, category, brand_name, id/sr no., 
  */
 
+const mongoose = require("mongoose");
+
+const product = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    category : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Category',
+        required : true
+    },
+    id : {
+        type : Number,
+        required : true,
+        autoIncrement : true
+    }
+})
